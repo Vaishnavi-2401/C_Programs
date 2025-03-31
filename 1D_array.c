7e7d997c39a3afeda61998ad610381a203f2f09e
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-03-31 12:15:01 +0530

    C_Programs

---
+++ 1D_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    printf("Elemets of an array : ");
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    
+    return 0;
+
+}
